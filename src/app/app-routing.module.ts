b0e02b9f555d4a1eb31bf03214a862a6cfd1f20b
Lalit Radadiya <radadiya7lalit7@gmail.com>
2021-02-07 17:43:21 +0530

    init first added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TokenstatsComponent } from './tokenstats/tokenstats.component';
+import { TokeninfoComponent } from './tokeninfo/tokeninfo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:HomeComponent},
+  {path:'reservation',component:ReservationComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'tokenstats',component:TokenstatsComponent},
+  {path:'tokeninfo',component:TokeninfoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingmod =[HomeComponent, ReservationComponent, DashboardComponent, TokenstatsComponent, TokeninfoComponent];
---
+++ src/app/tokeninfo/tokeninfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tokeninfo',
+  templateUrl: './tokeninfo.component.html',
+  styleUrls: ['./tokeninfo.component.scss']
+})
+export class TokeninfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
